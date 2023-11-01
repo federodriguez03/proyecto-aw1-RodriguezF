fb597d0db126eba5d2feef4eea7b62d1537c9254
root <mgiraudo@ies21.edu.ar>
2023-11-01 11:04:17 -0300

    etapa 4 JS

--- especialidades.js
+++ especialidades.js
@@ -1,26 +1,37 @@
 const especialidades = {
     categoria: "A-G",
     especialidades: [
-        {nombre: "Cardiología"},
-        {nombre: "Dermatología"},
-        {nombre: "Endocrinología"},
-        {nombre: "Gastroenterología"}
+        {
+            nombre: "Cardiología",
+            nombre: "Dermatología",
+            nombre: "Endocrinología",
+            nombre: "Gastroenterología"
+        }
     ],
     categoria: "H-O",
     especialidades: [
-        {nombre: "Hematología"},
-        {nombre: "Infectología"},
-        {nombre: "Nefrología"},
-        {nombre: "Oftalmología"}
+        {
+            nombre: "Hematología",
+            nombre: "Infectología",
+            nombre: "Nefrología",
+            nombre: "Oftalmología"
+        }    
     ],
     categoria: "P-U",
     especialidades: [
-        {nombre:  "Pediatría"},
-        {nombre: "Química Clínica"},
-        {nombre: "Reumatología"},
-        {nombre: "Urología"}
+        {
+            nombre: "Pediatría",
+            nombre: "Química Clínica",
+            nombre: "Reumatología",
+            nombre: "Urología"
+        }
     ],
 };
 
-let RenderizadoHTML = `<option>${especialidades.nombre}</option>`
+export default especialidades //Permite importar por otro lado
+
+//const esp = document.getElementById(especialidades)
+
+//let RenderizadoHTML = `<option>${especialidades.nombre}</option>`
+
 
